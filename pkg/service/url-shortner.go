5d83b2654f5348a0d453bfff1315686d2cac5283
Vibhor Dubey <vibhordubey333@gmail.com>
2022-11-12 08:06:24 +0530

    URL-Shortner-Service|Refactored go.mod to updated version of go, Added server, handlers code

---
+++ pkg/service/url-shortner.go
@@ -0,0 +1,31 @@
+package service
+
+import (
+	"math/rand"
+	"time"
+)
+
+const (
+	generatedKeySize = 6
+	chars            = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
+	charsLen         = len(chars)
+)
+
+type shortURLGenerator struct{}
+
+type URLKeyGenerator interface {
+	KeyGenerator() string
+}
+
+func NewURLKeyGenerator() URLKeyGenerator {
+	rand.Seed(time.Now().UnixNano())
+	return &shortURLGenerator{}
+}
+
+func (kg *shortURLGenerator) KeyGenerator() string {
+	b := make([]byte, generatedKeySize)
+	for i := range b {
+		b[i] = chars[rand.Intn(charsLen)]
+	}
+	return string(b)
+}
---
+++ server/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"vibhordubey333/URL-Shortner-Service/pkg/handler"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Welcome to the URL Shortener API\n1. /create-short-url\n2. /:shortUrl",
+		})
+	})
+
+	r.POST("/create-short-url", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
+	errorResponse := r.Run(":7777")
+	if errorResponse != nil {
+		panic(fmt.Sprintf("Error while starting URL-Shortner-Server%v", errorResponse))
+	}
+
+}
